9817b3044df6604925cf020d081195408fb76c83
Ismael Zeitoun Soumahoro <izsoum2024@gmail.com>
2023-03-02 23:17:15 -0500

    Issues with getting bootstrap to connect with ejs file

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs"); // allows for the use of ejs
 
-require("dotenv").config();
+require("dotenv").config(); // allows for use of .env to hide information. This is then added to the .gitignore
+
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
 
 app.get("/", function (req, res) {
-  res.render("index", { aVariable: "Ismael" });
+    res.render("index", { aVariable: "Ismael" });
 });
 
 app.listen(port, function () {
-  console.log("Server is listening on port " + port);
+    console.log("Server is listening on port " + port);
 });
--- views/src/script.js
+++ views/src/script.js
@@ -1,3 +1,5 @@
+import "../../node_modules/bootstrap/dist/js/bootstrap";
+
 var x = document.getElementById("demo");
 var latitude = "";
 var longitude = "";
