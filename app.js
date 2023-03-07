5ff29533ded9bb83ea39d58ca3ee553efa4a237e
Ismael Zeitoun Soumahoro <izsoum2024@gmail.com>
2023-03-07 16:50:01 -0500

    Got bootstrap to work :)

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+// jshint esversion:6
+
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs"); // allows for the use of ejs
-
 require("dotenv").config(); // allows for use of .env to hide information. This is then added to the .gitignore
 
-app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
-app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     res.render("index", { aVariable: "Ismael" });
